3127994ba8d58aa48c3b238a70c4fe2a212cca05
Soyeon <soyeon.kim@tum.de>
2023-08-27 16:00:59 +0200

    Update invert-binary-tree.java
    fixed time complexity to O(nlogn)

--- study1/week2/invert-binary-tree.java
+++ study1/week2/invert-binary-tree.java
@@ -18,7 +18,8 @@
 
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        //time complexity: O(n), because it visits n nodes
+        //time complexity: O(nlogn) bacause it is binary Tree archtecture 
+        //-> 1. first root -> 2. firstroot.left, firstroot.right -> 3. firstroot.left.left/right, firstroot.right.left/right ... 
         //space complexity: O(1), because of just changing?..
         if (root == null) {
            return root; //or null?

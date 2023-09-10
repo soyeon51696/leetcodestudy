7497398ae68defe6cad8c25c26a5af97a671dd77
Soyeon <soyeon.kim@tum.de>
2023-09-10 21:03:14 +0200

    Update reduce-array-size-to-the-half.java
    fixed the time complexity to O(nlogn) and added code for code review.

--- study1/week3/reduce-array-size-to-the-half.java
+++ study1/week3/reduce-array-size-to-the-half.java
@@ -2,11 +2,8 @@
 
 class Solution {
     public int minSetSize(int[] arr) {
-        //time complexity is O(n)
+        //time complexity is O(nlogn)
         //space complexity is also O(n)
-        
-        int arrLength = arr.length;
-        int result = 0;
 
         Map<Integer,Integer> map = new HashMap<Integer,Integer>();
 
@@ -27,18 +24,36 @@ class Solution {
         entryList.sort((a, b) -> b.getValue() - a.getValue());
 
         int removed = 0;
+        int halfArray = arr.length / 2;
+        int result = 0;
         int index = 0;
 
-        while (removed < arrLength / 2) {
+        while (removed < halfArray) {
             removed += entryList.get(index++).getValue();
-            if (index == arrLength) {
-                break;
-            }
+            // if (index == arrLength) {
+            //     break;
+            // } -> don't need it
             result++;
 
         }
 
         return result;      
+
+        /* code review
+        //After counting the numbers, the key actually becomes irrelevant, as we only need the count, 
+        //so you can make this a simple List with one Integer type in it, so :
+        List<Integer> entryList = new ArrayList<>(map.values());
+        Collections.sort(entryList);
+        //ㄴ> but as the sort() sort it as ascending order, you need to iterate backwards.
+
+        int removed = 0;
+        int halfArray = arr.length / 2; //Rather making arrLength a variable, making halfArray would seem to make more sense as it's being used multiple times.
+        int result = entryList.size() - 1;
+        while (removed < halfArray) {
+            removed += entryList.get(result--);
+        }
+        return entryList.size() - result - 1;
+        /*
         
     }
 }

50c1b347b7083a20f935bec8e5caaeee548e037f
Soyeon <soyeon.kim@tum.de>
2023-09-16 17:19:45 +0200

    Update max-area-of-island-nr.java
    added comments row and column

--- study1/week4/max-area-of-island-nr.java
+++ study1/week4/max-area-of-island-nr.java
@@ -5,14 +5,14 @@ class Solution {
     public int maxAreaOfIsland(int[][] grid) {
         //time complexity is O(n*m)
         //space complexity is O(n*m), because the queue can store at most O(n * m) elements
-        int n = grid.length;
-        int m = grid[0].length;
+        int m = grid.length; //row
+        int n = grid[0].length; //column
         int maxArea = 0;
         int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
 
-        for (int y = 0; y < n; y++) {
-            for (int x = 0; x < m; x++) {
+        for (int y = 0; y < m; y++) {
+            for (int x = 0; x < n; x++) {
                 if (grid[y][x] == 1) {
                     int area = 0;
                     Queue<int[]> queue = new LinkedList<>();
@@ -26,7 +26,7 @@ class Solution {
                             int newY = point[0] + dir[0];
                             int newX = point[1] + dir[1];
 
-                            if (newY >= 0 && newY < n && newX >= 0 && newX < m && grid[newY][newX] == 1) {
+                            if (newY >= 0 && newY < m && newX >= 0 && newX < n && grid[newY][newX] == 1) {
                                 queue.offer(new int[]{newY, newX});
                                 // change the cell as visited to 0
                                 grid[newY][newX] = 0;

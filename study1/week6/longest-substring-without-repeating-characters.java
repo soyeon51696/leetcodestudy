13aa5b9cc8ae6ac4ec1da174515de238bf46f1e7
Soyeon <soyeon.kim@tum.de>
2023-09-30 19:42:22 +0200

    Update longest-substring-without-repeating-characters.java
    Fixed space complexity and changed maxLegnth assignment after if/else.

--- study1/week6/longest-substring-without-repeating-characters.java
+++ study1/week6/longest-substring-without-repeating-characters.java
@@ -2,7 +2,7 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         //time complexity is O(n), where n is the lenfth of the String. 
-        //space complexity is O(n), where n is the size of the HashSet.
+        //space complexity is O(26), because Hashset will only contain 26 characters at the worst case
 
         int index = 0;
         int maxLength = 0;
@@ -11,15 +11,14 @@ class Solution {
         while (index < s.length()) {
             char c = s.charAt(index);
             if (set.contains(c)) {
-                maxLength = Math.max(maxLength, set.size());
                 set.remove(s.charAt(index - set.size()));;
             } 
             else {
                 set.add(c);
                 index++;
             }
+            maxLength = Math.max(maxLength, set.size());
         }
-        
-        return Math.max(maxLength, set.size());
+        return maxLength;
     }
 }
